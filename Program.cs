fce31d8fd25dafd7e67fcd3a32804b197bcfac9f
unknown <tiagocvalim@gmail.com>
2020-06-04 16:33:10 -0300

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula_10_Polimorfismo_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
